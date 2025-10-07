97927348ec90a6c931f23be25be3a40bf51fd8ef
pmoyer6673-hash <pmoyer6673@student.pps.net>
2025-10-07 10:01:38 -0700

    GUN
    Added tuaret

--- scripts/enemy_projectile.gd
+++ scripts/enemy_projectile.gd
@@ -1,7 +1,16 @@
 extends Area2D
+var speed = 400
+var direction = 0
+
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body):
 	pass
+	
+func _physics_process(delta: float):
+	position += speed * direction * delta
+	
+func set_direction(target):
+	direction = position.direction_to(target)
--- scripts/turret.gd
+++ scripts/turret.gd
@@ -1,21 +1,38 @@
 extends CharacterBody2D
-
+var Player
+var in_radius = false
+var startTime = 1
+var timer = startTime
+var projectile_original = preload("res://scenes/enemy_projectile.tscn")
 
 func _ready():
 	
 	pass
 
 func _process(delta: float) -> void:
-	pass
+	if in_radius == true:
+		if timer < 0:
+			shoot(Player)
+			timer = startTime
+		timer -= delta
 
 
 	
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	
+	if body.name == "Player":
+		Player = body
+		in_radius= true
 	pass # Replace with function body.
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	pass # Replace with function body.
+	if body.name == "Player":
+		in_radius = false
+
+func shoot(target):
+	var projectile_clone = projectile_original.instantiate()
+	projectile_clone.global_position = position
+	projectile_clone.set_direction(target.position)
+	get_tree().get_root().add_child(projectile_clone)
