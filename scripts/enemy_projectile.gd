b6235a34c8b3c82448cfbe348d3633116f8c74c8
pmoyer6673-hash <pmoyer6673@student.pps.net>
2025-10-09 09:47:25 -0700

    Death
     can now die

--- scripts/enemy_projectile.gd
+++ scripts/enemy_projectile.gd
@@ -7,7 +7,10 @@ func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body):
-	pass
+	if body.name == "Player":
+		body.change_health(-2)
+		queue_free()
+		pass
 	
 func _physics_process(delta: float):
 	position += speed * direction * delta
