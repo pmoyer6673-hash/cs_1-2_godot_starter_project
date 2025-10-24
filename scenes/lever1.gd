64f3e7cdbb1749633721cdddef5549599ca49f9a
pmoyer6673-hash <pmoyer6673@student.pps.net>
2025-10-24 09:56:13 -0700

    Lever
    Finally got the levers to respond

--- scenes/lever1.gd
+++ scenes/lever1.gd
@@ -1,2 +1,9 @@
 extends Area2D
 var Active = false
+
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		Active = true
+		queue_free()
--- scenes/lever_2.gd
+++ scenes/lever_2.gd
@@ -1,2 +1,9 @@
 extends Area2D
 var Active = false
+@onready var _animation_player: AnimatedSprite2D = $AnimatedSprite2D
+
+func update_animation():
+	if Active:
+		_animation_player.play("on")
+	else:
+		_animation_player.play("off")
