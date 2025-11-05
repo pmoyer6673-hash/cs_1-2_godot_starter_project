acee54ada19bd5815a4dcb8464efedfdb4491d4a
pmoyer6673-hash <pmoyer6673@student.pps.net>
2025-11-05 10:00:48 -0800

    they minos can shot
    And fast

--- scripts/melee_minotaur.gd
+++ scripts/melee_minotaur.gd
@@ -4,9 +4,13 @@ var in_range = false
 var asgoring = false
 var whack = false
 var speed = 250
+var projectile_original = preload("res://scenes/enemy_arrow.tscn")
+
 @onready var player: CharacterBody2D = %Player
 
 func _process(delta: float) -> void:
+	if in_range:
+		shoot()
 	pass
 
 func _on_mlelee_body_entered(body: Node2D) -> void:
@@ -33,3 +37,9 @@ func _on_nd_amendment_body_entered(body: Node2D) -> void:
 func _on_nd_amendment_body_exited(body: Node2D) -> void:
 	if body.name == "Player":
 		in_range = false
+
+func shoot():
+	var projectile_clone = projectile_original.instantiate()
+	projectile_clone.global_position = position
+	projectile_clone.set_direction(player.position)
+	get_tree().get_root().add_child(projectile_clone)
