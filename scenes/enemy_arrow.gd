c7561e3e07505fd20d2034df1cfa67a4a4801fda
pmoyer6673-hash <pmoyer6673@student.pps.net>
2025-11-07 09:57:26 -0800

    I tried
    Got timer working instead of everything else I was supposed to do

---
+++ scenes/enemy_arrow.gd
@@ -0,0 +1,19 @@
+extends Area2D
+var speed = 400.5
+var direction = 0
+
+
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+
+func _on_body_entered(body):
+	if body.name == "Player":
+		body.change_health(-2)
+		queue_free()
+		pass
+	
+func _physics_process(delta: float):
+	position += speed * direction * delta
+	
+func set_direction(target):
+	direction = position.direction_to(target)
--- scripts/melee_minotaur.gd
+++ scripts/melee_minotaur.gd
@@ -4,38 +4,50 @@ var in_range = false
 var asgoring = false
 var whack = false
 var speed = 250
+var startTime = 1
+var timer = startTime
 var projectile_original = preload("res://scenes/enemy_arrow.tscn")
 
 @onready var player: CharacterBody2D = %Player
 
 func _process(delta: float) -> void:
 	if in_range:
-		shoot()
+		if timer < 0:
+			shoot()
+			timer = startTime
+		timer -= delta
+
 	pass
 
 func _on_mlelee_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
+		player = body
 		whack = true
 
 func _on_mlelee_body_exited(body: Node2D) -> void:
 	if body.name == "Player":
+		player = body
 		whack = false
 
 func _on_doors_seek_theme_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
+		player = body
 		in_range = false
 		asgoring = true
 
 func _on_doors_seek_theme_body_exited(body: Node2D) -> void:
 	if body.name == "Player":
+		player = body
 		asgoring = false
 		
 func _on_nd_amendment_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
+		player = body
 		in_range = true
 
 func _on_nd_amendment_body_exited(body: Node2D) -> void:
 	if body.name == "Player":
+		player = body
 		in_range = false
 
 func shoot():
