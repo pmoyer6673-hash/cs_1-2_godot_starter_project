1d44e550f850c49e92416a3c735c01eebdc0d95c
pmoyer6673-hash <pmoyer6673@student.pps.net>
2025-11-14 09:58:59 -0800

    Finished mino
    except animation partially but frankly that was pain trying to figure out

--- scripts/melee_minotaur.gd
+++ scripts/melee_minotaur.gd
@@ -1,16 +1,34 @@
 extends CharacterBody2D
 
+var yDirection = 0
+var xDirection = 0
+var facing = "left"
 var in_range = false
 var asgoring = false
 var whack = false
 var speed = 250
 var startTime = 1
 var timer = startTime
+var startTime2 = 2
+var timer2 = startTime2
 var projectile_original = preload("res://scenes/enemy_arrow.tscn")
+@onready var _animation_player: AnimatedSprite2D = $AnimatedSprite2D
 
 @onready var player: CharacterBody2D = %Player
 
 func _process(delta: float) -> void:
+	velocity.x = xDirection * speed
+	velocity.y = yDirection * speed
+	if xDirection > 0:
+		facing = "right"
+	elif xDirection < 0:
+		facing = "left"
+	elif yDirection < 0:
+		facing = "up"
+	elif yDirection > 0:
+		facing = "down"
+	update_animation()
+	
 	if in_range:
 		if timer < 0:
 			shoot()
@@ -19,10 +37,19 @@ func _process(delta: float) -> void:
 	elif asgoring:
 		position += position.direction_to(player.position) *speed*delta
 	if whack:
-			
-			pass
+		if timer2 < 0:
+			player.change_health(-2)
+			timer2 = startTime2
+		timer2 -= delta
 
-
+func update_animation():
+	if whack:
+		_animation_player.play("attack_" + facing)
+	else:
+		if !in_range and !asgoring:
+			_animation_player.play("crossbow_idle_" + facing)
+		elif asgoring and !whack:
+			_animation_player.play("walk_" + facing)
 
 func _on_mlelee_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
